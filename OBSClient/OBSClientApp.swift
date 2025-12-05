93409ec865bb55b13eda116bef231f5954129de7
pnfzygrzgf-svg <andere_mail@example.com>
2025-12-05 16:52:09 +0100

    Initial Commit

---
+++ OBSClient/OBSClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  OBSClientApp.swift
+//  OBSClient
+//
+//  Created by Dominik Guggisberg on 05.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct OBSClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
